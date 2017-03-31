69ebd126090837d1c7143d158ba70f198e4e0734
fabiansep <fabian.sep@gmail.com>
2017-03-30 21:42:12 -0300

    Corrección parte
    Se ha arreglado la parte 1

--- parte2/laQuena/routes/index.js
+++ parte2/laQuena/routes/index.js
@@ -12,6 +12,13 @@ var bodegaCtrl            = require('../controllers/bodegas');
 module.exports = function (app) {
 
   var router = express.Router();
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   router.get('/', function(req, res) {
     res.sendFile("./public/index.html");
   });
