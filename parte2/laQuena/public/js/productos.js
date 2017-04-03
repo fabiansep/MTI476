7542cd5ae2c8fad3c27908a0d99ef03f0f96ca9d
fabiansep <fabian.sep@gmail.com>
2017-04-02 23:03:39 -0300

    F
    F

--- parte2/laQuena/public/js/productos.js
+++ parte2/laQuena/public/js/productos.js
@@ -1,6 +1,8 @@
     $(function() {
       console.log('hola');
-      $( "p" ).text( "The DOM is now loaded and can be manipulated." );
+      //$( "p" ).text( "The DOM is now loaded and can be manipulated." );
+      $('#loading').show();
+
       $.ajax(
             {
               type: "GET",
@@ -14,7 +16,26 @@
               dataType: "json",
               success: function (data) {
 
-                    alert('hola');
+                    var element = '';
+                    for (i = 0; i <data.length; i++){
+
+                        element = element + '<div class="panel panel-info" vocab="http://schema.org/" typeof="Product"> <!-- panel-info -->\n';
+                    	element = element + '	<div class="panel-heading">\n'
+                    	element = element + '		<h3 class="panel-title"><span property="name"> '+data[i].name+'</span></h3>\n';
+                    	element = element + '	</div>\n';
+                    	element = element + '	<div class="panel-body">\n';
+                    	element = element + '		<span property="description">'+data[i].history+'</span>\n';
+                    	element = element + '	</div> \n';
+                    	element = element + ' 	<div class="clasification">\n';
+                      for(j = 0; j < data[i].clasificationList.length;j++){
+                          element = element + '		<span class="badge badge-info clasification">'+data[i].clasificationList[j]+'</span>\n';
+                      }
+                    	element	= element +	'	</div>\n';
+                    	element = element + '</div>\n';
+                    }
+                    $('#loading').hide();
+                    $('#producto-list').append(element);
+                    //alert('hola');
                     console.log(data);
 
                     },
